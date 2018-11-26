6ace84378812ca07f28f09b12b457026af0b53a9
ytois <xxzoo200xx@gmail.com>
2018-11-26 13:34:54 +0900

    flask-webpack

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, Blueprint, render_template
+from flask_webpack import Webpack
 
-app = Flask(__name__)
+
+webpack = Webpack()
+
+
+def create_app(settings_override=None):
+    app = Flask(__name__)
+
+    params = {
+        'DEBUG': True,
+        'WEBPACK_MANIFEST_PATH': './static/manifest.json',
+    }
+
+    app.config.update(params)
+
+    if settings_override:
+        app.config.update(settings_override)
+
+    blueprint_js = Blueprint("javascript", __name__,
+                             static_url_path='/js', static_folder='./static/js')
+    blueprint_css = Blueprint(
+        "css", __name__, static_url_path='/css', static_folder='./static/css')
+
+    app.register_blueprint(blueprint_js)
+    app.register_blueprint(blueprint_css)
+
+    webpack.init_app(app)
+
+    return app
+
+
+app = create_app()
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+    # return app.send_static_file('index.html')
 
 
 if __name__ == '__main__':
-    app.debug = True
     app.run()
--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,21 @@
+var path = require('path');
+var ManifestRevisionPlugin = require('manifest-revision-webpack-plugin');
+
 module.exports = {
   pages: {
     index: {
       entry: 'frontend/main.js'
     }
   },
-  outputDir: 'backend/static'
+  outputDir: 'backend/static',
+  configureWebpack: {
+    plugins: [
+      new ManifestRevisionPlugin(
+        path.join('backend', 'static', 'manifest.json'),
+        {
+          rootAssetPath: './frontend'
+        }
+      )
+    ]
+  }
 };
