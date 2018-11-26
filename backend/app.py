e1b92abd6cdab8278c7cc8d01053d561ce8d46db
ytois <xxzoo200xx@gmail.com>
2018-11-26 13:50:00 +0900

    use template pug(jade)

--- backend/app.py
+++ backend/app.py
@@ -2,9 +2,6 @@ from flask import Flask, Blueprint, render_template
 from flask_webpack import Webpack
 
 
-webpack = Webpack()
-
-
 def create_app(settings_override=None):
     app = Flask(__name__)
 
@@ -18,6 +15,10 @@ def create_app(settings_override=None):
     if settings_override:
         app.config.update(settings_override)
 
+    # use template pug(jade)
+    app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
+
+    # routing js/css
     blueprint_js = Blueprint("javascript", __name__,
                              static_url_path='/js', static_folder='./static/js')
     blueprint_css = Blueprint(
@@ -26,6 +27,8 @@ def create_app(settings_override=None):
     app.register_blueprint(blueprint_js)
     app.register_blueprint(blueprint_css)
 
+    # use webpack
+    webpack = Webpack()
     webpack.init_app(app)
 
     return app
@@ -36,8 +39,7 @@ app = create_app()
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-    # return app.send_static_file('index.html')
+    return render_template('index.jade')
 
 
 if __name__ == '__main__':
