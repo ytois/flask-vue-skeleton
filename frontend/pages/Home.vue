819b55ff01c1aa674716af20ea33b8edc59deb49
ytois <xxzoo200xx@gmail.com>
2018-11-27 11:52:45 +0900

    convert html to pug

--- frontend/pages/Home.vue
+++ frontend/pages/Home.vue
@@ -1,8 +1,7 @@
-<template>
-  <div>
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+<template lang="pug">
+  div
+    img(alt='Vue logo' src='../assets/logo.png')
+    HelloWorld(msg='Welcome to Your Vue.js App')
 </template>
 
 <script>
