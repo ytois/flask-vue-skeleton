ca82b244e8daa7945b20fba60f1b01223807b737
ytois <xxzoo200xx@gmail.com>
2018-11-28 10:18:41 +0900

    add stylus

--- frontend/main.js
+++ frontend/main.js
@@ -2,10 +2,8 @@ import Vue from 'vue'
 import store from './store'
 import router from './router'
 import App from './pages/App'
-import Buefy from 'buefy'
-import 'buefy/dist/buefy.css'
+import './styles'
 
-Vue.use(Buefy)
 Vue.config.productionTip = false
 
 new Vue({
