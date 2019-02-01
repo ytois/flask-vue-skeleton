0fbfadf28b63b78b02a4862645af27a64de33629
ytois <xxzoo200xx@gmail.com>
2019-02-01 11:15:52 +0900

    wrap class

--- backend/app.py
+++ backend/app.py
@@ -1,41 +1,62 @@
+import os
+import sys
+
 from flask import Flask
 from flask_webpack import Webpack
-from routes import register_routes
-from models.database import init_db
+
 import models  # noqa
-import os
+from models.database import init_db
+from routes import register_routes
 
 
-def create_app():
-    app = Flask(__name__)
+class App:
+    @classmethod
+    def create(self):
+        return self._create_app()
 
-    # load configure
-    app.config.from_object('config.default')
+    @classmethod
+    def _create_app(self):
+        app = Flask(__name__)
 
-    if os.getenv('FLASK_ENV', 'development') == 'production':
-        app.config.from_object('config.production')
-    else:
-        app.config.from_object('config.development')
+        self._load_config(app)
+        self._register_path(app)
 
-    app.config.from_pyfile('secrets.py', silent=True)
+        # use template pug(jade)
+        app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
 
-    # use template pug(jade)
-    app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
+        # use webpack
+        webpack = Webpack()
+        webpack.init_app(app)
 
-    # use webpack
-    webpack = Webpack()
-    webpack.init_app(app)
+        # routing
+        register_routes(app)
 
-    # routing
-    register_routes(app)
+        # init db
+        init_db(app)
 
-    # init db
-    init_db(app)
+        return app
 
-    return app
+    @classmethod
+    def _load_config(self, app):
+        # load configure
+        app.config.from_object('config.default')
+
+        if os.getenv('FLASK_ENV', 'development') == 'production':
+            app.config.from_object('config.production')
+        else:
+            app.config.from_object('config.development')
+
+        app.config.from_pyfile('secrets.py', silent=True)
+
+    @classmethod
+    def _register_path(self, app):
+        for path in ['utils']:
+            sys.path.append(
+                os.path.join(app.root_path, path)
+            )
 
 
-app = create_app()
+app = App.create()
 
 
 if __name__ == '__main__':
