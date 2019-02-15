39d7c49bb8512e4bb2acc5fbfb54c9e7f0bf4795
ytois <xxzoo200xx@gmail.com>
2019-02-15 15:39:14 +0900

    update user model

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,5 +1,7 @@
 from models.database import db
 from datetime import datetime
+from flask import url_for
+import secrets
 
 
 class User(db.Model):
@@ -11,3 +13,18 @@ class User(db.Model):
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
     updated_at = db.Column(db.DateTime, nullable=False,
                            default=datetime.now, onupdate=datetime.now)
+    invite_code = db.Column(db.String(16), unique=True)
+
+    @classmethod
+    def create_invite(cls, name, email):
+        code = cls.generate_invite_code()
+        return User(name=name, email=email, invite_code=code)
+
+    @classmethod
+    def generate_invite_code(cls):
+        return secrets.token_hex(nbytes=16)
+
+    @property
+    def invite_url(self):
+        # TODO: tmp path
+        return url_for('/', code=self.invite_code)
